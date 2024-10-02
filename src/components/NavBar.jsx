1bef83cf0b1633018287c9f469cbb529d9595340
zyad87 <zyadt871@gmail.com>
2024-10-02 12:00:08 +0300

    V1

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,8 +7,7 @@ function NavBar() {
     <div className="navbar bg-base-300 ">
       <div className="flex-1">
         <div className="flex justify-center items-center text-xl ">
-          <img src={imageLogo} className="w-12 h-12 p-2" alt="" />
-          Final space
+          SHOP 
         </div>
       </div>
       <div className="flex-none mx-4 hover:bg-gray-700 p-3 rounded-lg"></div>
